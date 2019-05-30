707b242b309a9521c5f55e1d972be0da52d0aa0b
Alexandre Joubert <contact@joubert.dev>
2019-05-30 17:38:15 +0200

    INTER: forgot to keep up to date for a while...

--- local/scripts/updatePasswords.sh
+++ local/scripts/updatePasswords.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-pass init "$(cat /home/alex/.local/scripts/private/passwords_key)" 
-python2 /home/alex/.local/scripts/csv2pass.py -i /tmp/keepass.csv -r keepPass/;
+pass init "$(cat ~/.local/scripts/private/passwords_key)" 
+python2 ~/.local/scripts/csv2pass.py -i /tmp/keepass.csv -r keepPass/;
 rm /tmp/keepass.csv;
