3191e0e775f46f02ae525f23a58897fc282d6948
Alexandre Joubert <contact@joubert.dev>
2019-06-29 16:59:57 +0200

    INTER: add auto-screen detection for polybar

---
+++ config/polybar/launch.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch Polybar, using default config location ~/.config/polybar/config
+MONITOR=$(polybar -m|head -1|sed -e 's/:.*$//g') polybar first > /dev/null &>/dev/null &
+MONITOR_SECONDARY=$(polybar -m|tail -1|sed -e 's/:.*$//g') polybar second > /dev/null &>/dev/null &
+
+
+echo "Polybar launched..."
