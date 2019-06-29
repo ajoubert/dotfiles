1a8ed3e0bc1f0dd8d862f13344041642c33d0cfe
Alexandre Joubert <alex.joubert1@gmail.com>
2019-06-29 16:34:29 +0200

    MINOR: add auto-download of random wallpaper

--- local/scripts/randomBackground.sh
+++ local/scripts/randomBackground.sh
@@ -1,2 +1,13 @@
 #!/bin/zsh
-feh --recursive --no-fehbg --randomize --bg-fill ~/.wallpapers;
+DIRECTORY="~/.wallpapers"
+
+if [ -d "$DIRECTORY" ]; then
+    feh --recursive --no-fehbg --randomize --bg-fill ~/.wallpapers;
+else
+    mkdir /tmp/wallpapers;
+    file1=$(mktemp /tmp/wallpapers/wallpaper-XXX.jpg)
+    file2=$(mktemp /tmp/wallpapers/wallpaper-XXX.jpg)
+    curl -L --output $file1 'https://unsplash.it/1920/1080/?random'
+    curl -L --output $file2 'https://unsplash.it/1920/1080/?random'
+    feh --recursive --no-fehbg --randomize --bg-fill /tmp/wallpapers;
+fi
