4af21f1f8ef96b70e0b727f913397e426b14eddc
Alexandre Joubert <alex.joubert1@gmail.com>
2019-06-29 16:14:25 +0200

    INTER: add background creation scripts

---
+++ local/scripts/randomBgAndVideo.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+killall xwinwrap > /dev/null 2> /dev/null;
+switchValue=$((RANDOM%4))
+## 1 -> static/static 
+## 2 -> static/video
+## 3 -> video/static
+## 4 -> video/video
+case $switchValue in
+0)
+    ## Static / static 
+    $HOME/.local/scripts/randomBackground.sh > /dev/null 2> /dev/null;
+;;
+1)
+    ## Static / video
+    $HOME/.local/scripts/randomVideo -w 1920 -h 1080 -x 1680 > /dev/null 2> /dev/null;
+    $HOME/.local/scripts/randomBackground.sh > /dev/null 2> /dev/null;
+;;
+2) ## video / static
+    $HOME/.local/scripts/randomVideo.sh -w 1680 -h 1050 > /dev/null 2> /dev/null;
+    $HOME/.local/scripts/randomBackground.sh > /dev/null 2> /dev/null;
+;;
+3)
+    ## video / video 
+    $HOME/.local/scripts/randomVideo.sh -w 1680 -h 1050 > /dev/null 2> /dev/null;
+    $HOME/.local/scripts/randomVideo.sh -w 1920 -h 1080 -x 1680 > /dev/null 2> /dev/null;
+;;
+esac
---
+++ local/scripts/randomVideo.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+## Set default values
+OFFSETX=0
+OFFSETY=0
+## Get options
+while getopts w:h:x:y: option
+do
+    case "${option}"
+        in
+        w) WIDTH=${OPTARG};;
+        h) HEIGHT=${OPTARG};;
+        x) OFFSETX=${OPTARG};;
+        y) OFFSETY=${OPTARG};;
+    esac
+done
+
+## Load a random animated image
+file=$(find -L ~/.wallpapers | grep mp4 | shuf -n1)
+## Display random animated image
+xwinwrap -ov -g ${WIDTH}x${HEIGHT}+${OFFSETX}+${OFFSETY} -ni -nf -- mpv -wid WID $file --no-osc --no-osd-bar --loop-file --player-operation-mode=cplayer --no-audio --panscan=1.0 --no-keepaspect --no-input-default-bindings --hwdec&
