41d2c6912bd7c02a426a4fb425aaadd2b3147fbf
Alexandre Joubert <alex.joubert1@gmail.com>
2019-07-31 21:07:15 +0200

    INTER : add theme switching for vim

--- local/scripts/switchtheme.sh
+++ local/scripts/switchtheme.sh
@@ -4,3 +4,4 @@
 ~/.tmux/bin/update_tmux_theme.sh $1 > /dev/null;
 ~/.config/polybar/bin/switchtheme.sh $1 > /dev/null &> /dev/null;
 ~/.local/scripts/switch_background_theme.sh $1 > /dev/null &> /dev/null;
+~/.config/vim/bin/switchtheme.sh $1 > /dev/null &> /dev/null;
