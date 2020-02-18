d0919c1d20460d55324412e12da1d2a6b2f7ba21
Alexandre Joubert <contact@joubert.dev>
2020-02-18 08:13:40 +0100

    MINOR : add option to clean background to randomBackground.sh

--- local/scripts/randomBackground.sh
+++ local/scripts/randomBackground.sh
@@ -7,9 +7,13 @@ else
     if [ ! -d /tmp/wallpapers ]; then
       mkdir /tmp/wallpapers;
     fi
+
+    if [[ $* == *--clean* ]]; then
+      rm /tmp/wallpapers/*;
+    fi
     file1=$(mktemp /tmp/wallpapers/wallpaper-XXX.jpg)
-    file2=$(mktemp /tmp/wallpapers/wallpaper-XXX.jpg)
+    ## file2=$(mktemp /tmp/wallpapers/wallpaper-XXX.jpg)
     curl -L --output $file1 'https://unsplash.it/1920/1080/?random'
-    curl -L --output $file2 'https://unsplash.it/1920/1080/?random'
+    ## curl -L --output $file2 'https://unsplash.it/1920/1080/?random'
     feh --recursive --no-fehbg --randomize --bg-fill /tmp/wallpapers;
 fi
