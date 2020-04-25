3f521090e365b8d76aafedbf3f134d89907ebf75
Alexandre Joubert <alex.joubert1@gmail.com>
2020-04-25 18:43:15 +0200

    DEBUG : fix bgAndVideo script for single screen setup

--- local/scripts/randomBgAndVideo.sh
+++ local/scripts/randomBgAndVideo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 killall xwinwrap > /dev/null 2> /dev/null;
-switchValue=$((RANDOM%4))
+switchValue=$((RANDOM%2))
 ## 1 -> static/static 
 ## 2 -> static/video
 ## 3 -> video/static
@@ -12,16 +12,16 @@ case $switchValue in
 ;;
 1)
     ## Static / video
-    $HOME/.local/scripts/randomVideo -w 1920 -h 1080 -x 1680 > /dev/null 2> /dev/null;
+    $HOME/.local/scripts/randomVideo.sh -w 1920 -h 1080 > /dev/null 2> /dev/null;
     $HOME/.local/scripts/randomBackground.sh > /dev/null 2> /dev/null;
 ;;
-2) ## video / static
-    $HOME/.local/scripts/randomVideo.sh -w 1680 -h 1050 > /dev/null 2> /dev/null;
-    $HOME/.local/scripts/randomBackground.sh > /dev/null 2> /dev/null;
-;;
-3)
-    ## video / video 
-    $HOME/.local/scripts/randomVideo.sh -w 1680 -h 1050 > /dev/null 2> /dev/null;
-    $HOME/.local/scripts/randomVideo.sh -w 1920 -h 1080 -x 1680 > /dev/null 2> /dev/null;
-;;
+# 2) ## video / static
+    # $HOME/.local/scripts/randomVideo.sh -w 1680 -h 1050 > /dev/null 2> /dev/null;
+    # $HOME/.local/scripts/randomBackground.sh > /dev/null 2> /dev/null;
+# ;;
+# 3)
+    # video / video 
+    # $HOME/.local/scripts/randomVideo.sh -w 1680 -h 1050 > /dev/null 2> /dev/null;
+    # $HOME/.local/scripts/randomVideo.sh -w 1920 -h 1080 -x 1680 > /dev/null 2> /dev/null;
+# ;;
 esac
