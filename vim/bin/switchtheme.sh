8abdf4895057b0d8ff1f925dc0830f49289a8d2b
Alexandre Joubert <contact@joubert.dev>
2020-04-27 15:09:43 +0200

    MINOR : auto-override vim-colors-solarized on update

--- vim/bin/switchtheme.sh
+++ vim/bin/switchtheme.sh
@@ -3,14 +3,14 @@
 
 if [ "$1" == "light" ]
 then
-  rm ~/.config/vim/config.vim;
-  cat ~/.config/vim/config.light.theme >> ~/.config/vim/config.vim;
-  cat ~/.config/vim/config.general >> ~/.config/vim/config.vim;
+  rm $VIM_CONFIG/config.vim;
+  cat $VIM_CONFIG/config.light.theme >> $VIM_CONFIG/config.vim;
+  cat $VIM_CONFIG/config.general >> $VIM_CONFIG/config.vim;
 elif [ "$1" == "dark" ]
 then
-  rm ~/.config/vim/config.vim;
-  cat ~/.config/vim/config.dark.theme >> ~/.config/vim/config.vim;
-  cat ~/.config/vim/config.general >> ~/.config/vim/config.vim;
+  rm $VIM_CONFIG/config.vim;
+  cat $VIM_CONFIG/config.dark.theme >> $VIM_CONFIG/config.vim;
+  cat $VIM_CONFIG/config.general >> $VIM_CONFIG/config.vim;
 else
   echo "Unknown theme...";
 fi
