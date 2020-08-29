9a906c614ab896929606e2d81f73716f96ded0c1
Alexandre Joubert <alex.joubert1@gmail.com>
2020-08-29 22:53:43 +0200

    MINOR : fix keepassxc display

--- config/shell/aliases.sh
+++ config/shell/aliases.sh
@@ -16,6 +16,7 @@ alias cdunrealengine="cd $HOME/documents/unreal_engine/UnrealEngine;"
 alias wiki="cd $HOME/.vimwiki/wiki/; clear;";
 alias workspace="cd $HOME/workspace/; clear;";
 alias getkey="keepassxc-cli clip $HOME/sync/keepass.kdbx localPwd"
+alias keepassxc="QT_AUTO_SCREEN_SCALE_FACTOR=0 keepassxc"
 onlinux &&  alias ls='ls -h --color=auto'
 onmac &&  alias ls='ls -G'
 
