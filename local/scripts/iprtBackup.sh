f218e762c6f86c73a98dfd6cc165cb2cc9bdfbc9
Alexandre Joubert <alex.joubert1@gmail.com>
2021-03-10 23:26:27 +0100

    MINOR : update backup script to automatically pull images

--- local/scripts/iprtBackup.sh
+++ local/scripts/iprtBackup.sh
@@ -1,19 +1,30 @@
 #!/bin/bash
 
-## First let's select everything we deem worth backing up
-## Then let's open the virtual file system
+IMG_FILE="$HOME/documents/iprt.img"
+
+## First let's check whether the file exists
+if [ -f "$IMG_FILE" ]; then
+    echo "Using $IMG_FILE";
+else 
+    echo "iprt.img not found, pulling to $IMG_FILE"
+    cd ~/workspace/gdrive;
+    node ~/workspace/gdrive/index.js -f ~/documents/iprt.img -n iprt.img -D -d;
+fi
+
+## Now let's open that file
 sudo cryptsetup open $HOME/documents/iprt.img iprt
 sudo mount /dev/mapper/iprt /mnt/secret
+
 ## Copy the content into the virtual file system
 gpg -a --export-filter drop-subkey='expired -t' --export >/mnt/secret/data/mypubkeys.asc
 gpg -a --export-filter drop-subkey='expired -t' --export-secret-keys >/mnt/secret/data/myprivatekeys.asc
 gpg --export-ownertrust >/mnt/secret/data/otrust.txt
 sudo cp ~/documents/iprt/* /mnt/secret/data/
-## Close the file system
 
+## Close the file system
 sudo umount /mnt/secret
 sudo cryptsetup close iprt
 
 ## Send it over to gdrive
 cd ~/workspace/gdrive;
-node ~/workspace/gdrive/index.js -f ~/documents/iprt/iprt.img -n iprt.img -d;
+node ~/workspace/gdrive/index.js -f ~/documents/iprt.img -n iprt.img -d;
