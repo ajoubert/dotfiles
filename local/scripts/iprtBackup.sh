645b7d86bf437f811de6a2437d0148475005a8b8
Alexandre Joubert <alex.joubert1@gmail.com>
2021-03-11 08:34:31 +0100

    MINOR : update gdrive location in iprtBackup script

--- local/scripts/iprtBackup.sh
+++ local/scripts/iprtBackup.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 IMG_FILE="$HOME/documents/iprt.img"
+GDRIVE_LOCATION="/mnt/hdd/data/workspace_archived/gdrive"
 
 ## First let's check whether the file exists
 if [ -f "$IMG_FILE" ]; then
     echo "Using $IMG_FILE";
 else 
     echo "iprt.img not found, pulling to $IMG_FILE"
-    cd ~/workspace/gdrive;
-    node ~/workspace/gdrive/index.js -f ~/documents/iprt.img -n iprt.img -D -d;
+    pushd $GDRIVE_LOCATION;
+    node $GDRIVE_LOCATION/index.js -f ~/documents/iprt.img -n iprt.img -D -d;
+    popd;
 fi
 
 ## Now let's open that file
@@ -26,5 +28,6 @@ sudo umount /mnt/secret
 sudo cryptsetup close iprt
 
 ## Send it over to gdrive
-cd ~/workspace/gdrive;
-node ~/workspace/gdrive/index.js -f ~/documents/iprt.img -n iprt.img -d;
+pushd $GDRIVE_LOCATION;
+node $GDRIVE_LOCATION/index.js -f ~/documents/iprt.img -n iprt.img -d;
+popd;
