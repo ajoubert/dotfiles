a4ec40f02b823148e0c76767a427c707add6ab92
Alexandre Joubert <contact@joubert.dev>
2021-05-20 19:26:25 +0200

    MINOR : add support for headset commands

---
+++ local/scripts/mpc_next.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+notify-send -t 1000 'Next' 'Instructing mpc to play the next song' --icon=dialog-information
+mpc next 
---
+++ local/scripts/mpc_pause.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+notify-send -t 1000 'Pause' 'Instructing mpc to pause' --icon=dialog-information
+mpc pause 
