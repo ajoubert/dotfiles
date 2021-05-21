237882ceb05a813c774de4709f3ae4c50b51d430
Alexandre Joubert <contact@joubert.dev>
2021-05-21 22:13:21 +0200

    MINOR : fix cover_obs.sh script

--- config/ncmpcpp/cover_obs.sh
+++ config/ncmpcpp/cover_obs.sh
@@ -2,14 +2,21 @@
 
 COVER="/tmp/album_cover.png"
 COVER_SIZE="400"
+MUSIC_DIR="/mnt/hdd/data/music/"
+
+rm /tmp/extract.jpg >/dev/null &>/dev/null
 
 #path to current song
 file="$MUSIC_DIR/$(mpc --format %file% current)"
 album="${file%/*}"
 #search for cover image
-art=$(find "$album"  -maxdepth 1 | grep -m 1 ".*\.\(jpg\|png\|gif\|bmp\)")
-if [ "$art" = "" ]; then
-  art="$HOME/.ncmpcpp/default_cover.png"
+ffmpeg -i $file -an -vcodec copy /tmp/extract.jpg >/dev/null &>/dev/null
+
+if [ -f "/tmp/extract.jpg" ]; then
+  art="/tmp/extract.jpg"
+else
+  art="$HOME/.config/ncmpcpp/default_cover.png"
 fi
+
 #copy and resize image to destination
 ffmpeg -loglevel 0 -y -i "$art" -vf "scale=$COVER_SIZE:-1" "$COVER"
