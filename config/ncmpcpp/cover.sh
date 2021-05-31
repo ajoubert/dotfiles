85c7890ea4edac7b0910d8e9474196da6cb5fea0
Alexandre Joubert <alex.joubert1@gmail.com>
2021-05-31 18:53:14 +0200

    MINOR : fix ncmpcpp music alias

--- config/ncmpcpp/cover.sh
+++ config/ncmpcpp/cover.sh
@@ -6,12 +6,30 @@
 
 source "`ueberzug library`"
 
-COVER="/tmp/album_cover.png"
+COVER="/tmp/album_cover.jpg"
 X_PADDING=0
-Y_PADDING=3
+Y_PADDING=0
+WIDTH=49
+HEIGHT=19
 
 function add_cover() {
-    ImageLayer::add [identifier]="cover" [scaler]="fit_contain" [x]="$X_PADDING" [y]="$Y_PADDING" [width]="49" [height]="23" [path]="$COVER"
+    IMAGE_WIDTH=$(identify -format "%w" /tmp/album_cover.jpg)> /dev/null
+    IMAGE_HEIGHT=$(identify -format "%h" /tmp/album_cover.jpg)> /dev/null
+    RATIO_WIDTH=$(echo "scale=4; $WIDTH/$IMAGE_WIDTH" | bc)
+    RATIO_HEIGHT=$(echo "scale=4; ($HEIGHT*2)/$IMAGE_HEIGHT" | bc)
+    if (( $(bc <<< "$RATIO_WIDTH >= $RATIO_HEIGHT") )); then
+        CHOSEN_RATIO=$RATIO_WIDTH;
+    else
+        CHOSEN_RATIO=$RATIO_HEIGHT;
+    fi
+    MODIFIED_WIDTH=$(echo "scale=4; $IMAGE_WIDTH * $CHOSEN_RATIO" | bc)
+    OFFSET_WIDTH=$(echo "scale=0; ($WIDTH - $MODIFIED_WIDTH) / 2" | bc)
+
+    MODIFIED_HEIGHT=$(echo "scale=4; ($IMAGE_HEIGHT * $CHOSEN_RATIO)" | bc)
+    OFFSET_HEIGHT=$(echo "scale=0; ($HEIGHT - $MODIFIED_HEIGHT) / 2" | bc)
+
+    echo "Offset: $OFFSET_WIDTH || $OFFSET_HEIGHT" > /tmp/values.tmp
+    ImageLayer::add [identifier]="cover" [scaler]="fit_contain" [x]="$X_PADDING" [y]="$Y_PADDING" [width]="$WIDTH" [height]="$HEIGHT" [path]="$COVER"
 }
 
 function remove_cover() {
