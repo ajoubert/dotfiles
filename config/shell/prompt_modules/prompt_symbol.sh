a2338496ff5eb81389fc04e9f57666b508bc7083
Alexandre Joubert <contact@joubert.dev>
2021-07-16 23:19:20 +0200

    MINOR : fix prompt

--- config/shell/prompt_modules/prompt_symbol.sh
+++ config/shell/prompt_modules/prompt_symbol.sh
@@ -3,8 +3,11 @@
 
 function set_prompt_symbol () {
   if test $1 -eq 0 ; then
-      PROMPT_SYMBOL="\[${base00}\]➜\[${COLOR_NONE}\] "
+      #PROMPT_SYMBOL="\[${base00}\]➜\[${COLOR_NONE}\] "
+      ## For some reason, the use of COLOR_NONE breaks the term when moving back in the history...
+      PROMPT_SYMBOL="➜ "
   else
+      #PROMPT_SYMBOL="\[${COLOR_RED}\]➜\[${COLOR_NONE}\] "
       PROMPT_SYMBOL="\[${COLOR_RED}\]➜\[${COLOR_NONE}\] "
   fi
 }
