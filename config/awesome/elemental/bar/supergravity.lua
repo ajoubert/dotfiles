a5875bcb6eec7a0f7a678cf74104b3b331e39fcc
Alexandre Joubert <contact@joubert.dev>
2022-06-18 17:21:13 +0200

    MINOR : remove dock config in awesomewm

--- config/awesome/elemental/bar/supergravity.lua
+++ config/awesome/elemental/bar/supergravity.lua
@@ -7,26 +7,7 @@ local helpers = require("helpers")
 local keys = require("config/keys")
 local t = require("config/tags")
 
-local dock_autohide_delay = 0.5 -- seconds
-
 -- {{{ Widgets
-local update_taglist = function (item, tag, index)
-    if tag.selected then
-        item.bg = beautiful.taglist_text_color_focused[index]
-    elseif tag.urgent then
-        item.bg = beautiful.taglist_text_color_urgent[index]
-    elseif #tag:clients() > 0 then
-        item.bg = beautiful.taglist_text_color_occupied[index]
-    else
-        item.bg = beautiful.taglist_text_color_empty[index]
-    end
-end
-
-local dock = require("noodle.dock")
-local dock_placement = function(w)
-    return awful.placement.bottom(w)
-end
-
 local tag_colors_empty = { "#00000000", "#00000000", "#00000000", "#00000000", "#00000000", "#00000000", "#00000000", "#00000000", "#00000000", "#00000000" }
 
 local tag_colors_urgent = { X.foreground, X.foreground, X.foreground, X.foreground, X.foreground, X.foreground, X.foreground, X.foreground, X.foreground, X.foreground }
@@ -115,93 +96,6 @@ awful.screen.connect_for_each_screen(function(s)
         widget = s.mytaglist,
     }
 
-    -- Create the dock wibox
-    s.dock = awful.popup({
-        -- Size is dynamic, no need to set it here
-        visible = false,
-        bg = "#000000",
-        -- bg = "#00000000",
-        ontop = true,
-        type = "dock",
-        placement = dock_placement,
-        widget = dock,
-        screen = s
-    })
-    dock_placement(s.dock)
-
-    local popup_timer
-    local autohide = function ()
-        if popup_timer then
-            popup_timer:stop()
-            popup_timer = nil
-        end
-        popup_timer = gears.timer.start_new(dock_autohide_delay, function()
-            popup_timer = nil
-            s.dock.visible = false
-        end)
-    end
-
-    -- Initialize wibox activator
-    s.dock_activator = wibox({ screen = s, height = 1, bg = "#00000000", visible = true, ontop = true})
-    awful.placement.bottom(s.dock_activator)
-    s.dock_activator:connect_signal("mouse::enter", function()
-        s.dock.visible = true
-        if popup_timer then
-            popup_timer:stop()
-            popup_timer = nil
-        end
-    end)
-
-    -- We have set the dock_activator to be ontop, but we do not want it to be
-    -- above fullscreen clients
-    local function no_dock_activator_ontop(c)
-        if c.fullscreen then
-            s.dock_activator.ontop = false
-        else
-            s.dock_activator.ontop = true
-        end
-    end
-    client.connect_signal("focus", no_dock_activator_ontop)
-    client.connect_signal("unfocus", no_dock_activator_ontop)
-    client.connect_signal("property::fullscreen", no_dock_activator_ontop)
-
-    s.dock_activator:buttons(
-        gears.table.join(
-            awful.button({ }, 4, function ()
-                awful.tag.viewprev()
-            end),
-            awful.button({ }, 5, function ()
-                awful.tag.viewnext()
-            end)
-    ))
-
-    local function adjust_dock()
-        -- Reset position every time the number of dock items changes
-        dock_placement(s.dock)
-
-        -- Adjust activator width every time the dock wibox width changes
-        s.dock_activator.width = s.dock.width + Dpi(250)
-        -- And recenter
-        awful.placement.bottom(s.dock_activator)
-    end
-
-    adjust_dock()
-    s.dock:connect_signal("property::width", adjust_dock)
-
-    s.dock:connect_signal("mouse::enter", function ()
-        if popup_timer then
-            popup_timer:stop()
-            popup_timer = nil
-        end
-    end)
-
-    s.dock:connect_signal("mouse::leave", function ()
-        autohide()
-    end)
-    s.dock_activator:connect_signal("mouse::leave", function ()
-        autohide()
-    end)
-
     -- Create a system tray widget
     s.systray = wibox.widget.systray()
     -- Create the tray box
@@ -228,17 +122,6 @@ awful.screen.connect_for_each_screen(function(s)
         end)
     ))
 end)
-
-awesome.connect_signal("elemental::dismiss", function()
-    local s = mouse.screen
-    s.dock.visible = false
-end)
-
--- Every bar theme should provide these fuctions
-function wibars_toggle()
-    local s = awful.screen.focused()
-    s.dock.visible = not s.dock.visible
-end
 
 function Tray_toggle()
     local s = awful.screen.focused()
