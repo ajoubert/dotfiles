ad8927e3ccb813cf4b0f0dcfdd3eacd94e0dc949
Alexandre Joubert <contact@joubert.dev>
2022-09-01 20:37:19 +0200

    MAJOR : remove unused config (bspwm and co)

--- config/termite/bin/generate_config_file.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-termitedir="$( cd "$(dirname "$0")"; cd .. ; pwd -P )"
-rm $termitedir/config;
-touch $termitedir/config;
-cat $termitedir/config_colors >> $termitedir/config;
-cat $termitedir/config_general >> $termitedir/config;
-cat $termitedir/config;
-echo "All done, see output above";
