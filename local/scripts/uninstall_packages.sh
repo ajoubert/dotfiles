50a3725d76539fb07ee83ee2c9e5dc29faf54f3c
Alexandre Joubert <contact@joubert.dev>
2024-01-01 20:22:12 +0100

    MINOR : improved uninstall_packages script

--- local/scripts/uninstall_packages.sh
+++ local/scripts/uninstall_packages.sh
@@ -1,54 +1,23 @@
 #!/bin/bash
 
-# File containing the list of packages
-FILE="$HOME/.config/pkglist.txt"
-
 # Function to display usage
 usage() {
-    echo "Usage: $0 [--dry-run]"
+    echo "Uninstall all packages not explicitely defined in pkglist.txt"
     exit 1
 }
 
-# Check for --dry-run option
-DRY_RUN=0
-if [ "$1" == "--dry-run" ]; then
-    DRY_RUN=1
-elif [ -n "$1" ]; then
+if [ -n "$1" ]; then
     usage
 fi
 
-# Check if packages.txt exists
-if [ ! -f $FILE ]; then
-    echo "Error: pkglist.txt not found."
-    exit 1
+# File containing the list of packages
+FILE="$HOME/.config/pkglist.txt"
+
+# If file doesn't exist, exit with an error
+if [ ! -f "$FILE" ]; then
+  echo "Error: pkglist.txt not found."
+  exit 1
 fi
 
-# Read all explicitly installed packages
-explicit_packages=$(yay -Qe | awk '{print $1}')
-
-# Initialize an empty string for unlisted packages
-unlisted_packages=""
-
-# Loop through each explicitly installed package
-for pkg in $explicit_packages; do
-    # Check if the package is in packages.txt
-    if ! grep -qx "$pkg" $FILE; then
-        # Append the package to the unlisted packages string
-        unlisted_packages+="$pkg "
-    fi
-done
-
-# Check if there are any unlisted packages
-if [ -z "$unlisted_packages" ]; then
-    echo "No packages to remove."
-    exit 0
-fi
-
-# If dry run, just display the packages
-if [ $DRY_RUN -eq 1 ]; then
-    echo "Packages that would be removed:"
-    echo $unlisted_packages
-else
-    # Uninstall all unlisted packages
-    yay -Rns $unlisted_packages
-fi
+# Uninstall all unlisted packages
+yay -Rsu $(comm -23 <(yay -Qq | sort) <(sort $FILE))
