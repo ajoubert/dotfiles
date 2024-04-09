b96e4ac53eec6dc839993b82955735ade235341e
Alexandre Joubert <contact@joubert.dev>
2024-04-09 18:14:03 +0200

    MINOR : fix thumbnailify

--- local/scripts/thumbnailify.sh
+++ local/scripts/thumbnailify.sh
@@ -14,11 +14,11 @@ if ! [ -x "$(command -v ffmpeg)" ]; then
   exit 1
 fi
 
-while getopts "ho:v" opt; do
+while getopts "ht:o:" opt; do
     case "${opt}" in
         h)  # Show help message
             echo "Usage: $0 [-o <output_filename>] [-t <time-of-screenshot>] <input_video>" 
-            exit 1
+            exit 0
             ;;
         o)  # Specify output file
             output_file=${OPTARG}
@@ -36,21 +36,21 @@ done
 shift $((OPTIND-1))
 
 # Check if argument for video file is passed
-if [ -z $1 ]; then
+if [ -z "$1" ]; then
   echo "Error: No input video file specified." >&2
   exit 1
 fi
 
 # Check if input video file exists on filesystem
-if [ ! -f $1 ]; then
+if [ ! -f "$1" ]; then
   echo "Error: $1 does not exist." >&2
   exit 1
 fi
 
 # Check if output file is specified
-if [ -z $output_file ]; then
-  output_file=$(dirname $1)"/"$(basename $1 .*)".jpg"
+if [ -z "$output_file" ]; then
+  output_file="$(dirname "$1")/$(basename "$1" .*)".jpg""
 fi
 
 # Generate the thumbnail
-ffmpeg -i $1 -ss $time_in_seconds -vframes 1 $output_file
+ffmpeg -i "$1" -ss $time_in_seconds -vframes 1 "$output_file"
