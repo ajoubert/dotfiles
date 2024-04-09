c692348c8f95b4532b1da344bdc32091f37a7b3f
Alexandre Joubert <contact@joubert.dev>
2024-04-09 18:14:57 +0200

    MINOR : update changed binary

--- local/scripts/yt-dl-mp3.sh
+++ local/scripts/yt-dl-mp3.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-youtube-dl --audio-quality 0 -i --extract-audio --audio-format mp3 -o './%(title)s.%(ext)s' --add-metadata --embed-thumbnail --metadata-from-title "%(artist)s - %(title)s" $1
+yt-dlp --audio-quality 0 -i --extract-audio --audio-format mp3 -o './%(title)s.%(ext)s' --add-metadata --embed-thumbnail --metadata-from-title "%(artist)s - %(title)s" $1
