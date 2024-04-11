43a14adcfde7141a51ecf8b3d7f26124b45a5f01
Alexandre Joubert <contact@joubert.dev>
2024-04-11 21:22:50 +0200

    MINOR : change hostname of ams when remote mounting

--- local/scripts/mount_remote_ams.sh
+++ local/scripts/mount_remote_ams.sh
@@ -2,4 +2,4 @@
 #ansible-playbook $ANSIBLE_DATA/inventory/playbooks/ovh/serv-0/activate_forwarding.yml;
 #sshfs sloth@ams:/media/sloth/storage/a/ /mnt/remotes/ams/ -o allow_other
 
-sshfs sloth@ams-direct-eth:/media/sloth/storage1/a/ /mnt/remotes/ams/ -o allow_other
+sshfs sloth@ams-direct:/media/sloth/storage1/a/ /mnt/remotes/ams/ -o allow_other
