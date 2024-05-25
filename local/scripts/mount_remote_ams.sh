bb1c2f8b84361c85837a8c7ec4907b38b7c7fc66
Alexandre Joubert <contact@joubert.dev>
2024-05-25 08:56:50 +0200

    MINOR : update remote mount scripts

--- local/scripts/mount_remote_ams.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-#ansible-playbook $ANSIBLE_DATA/inventory/playbooks/ovh/serv-0/activate_forwarding.yml;
-#sshfs sloth@ams:/media/sloth/storage/a/ /mnt/remotes/ams/ -o allow_other
-
-sshfs sloth@ams-direct:/media/sloth/storage1/a/ /mnt/remotes/ams/ -o allow_other
