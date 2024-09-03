21d2539c8ddbe83a112bcf179d9c77bfa3b778ac
Alexandre Joubert <contact@joubert.dev>
2024-09-03 18:34:01 +0200

    MINOR : update ansible forwarding scripts

--- local/scripts/disableForwardingWithAnsible.sh
+++ local/scripts/disableForwardingWithAnsible.sh
@@ -1,13 +1,38 @@
 #!/bin/bash
 
-# Check if the ansible identities script is executable
-if [ ! -x "ssh-add-ansible-identities.sh" ]; then
-    echo "ssh-add-ansible-identities.sh is not available"
+if [ -z "$KEEPASSXC_DATABASE_PATH" ]; then
+    echo "KEEPASSXC_DATABASE_PATH is not set"
     exit 1
 fi
 
-ssh-add-ansible-identities.sh
+if [ -z "$ANSIBLE_SUDO_PASS_ENTRY" ]; then
+    echo "ANSIBLE_SUDO_PASS_ENTRY is not set"
+    exit 1
+fi
 
-ansible-playbook $PLAYBOOKS_HOME/ovh/serv-0/deactivate_forwarding.yml;
+# Check if ssh-add-ansible-identities.sh binary is available on current
+# shell
+if ! which ssh-add-ansible-identities.sh > /dev/null &>/dev/null; then
+    echo "ssh-add-ansible-identities.sh is not available on current shell"
+    exit 1
+fi
+
+# Checks if password of database is passed as $1
+if [ -z "$1" ]; then
+  echo -n "Enter KeePassXC database password: "
+  read -s KP_PASSWORD
+  echo
+else
+  KP_PASSWORD="$1"
+fi
+
+# Get identities, exit early if it fails
+if ! ssh-add-ansible-identities.sh $KP_PASSWORD; then
+    exit 1
+fi
+
+SUDOPASS=$(echo "$KP_PASSWORD" | keepassxc-cli show -s "$KEEPASSXC_DATABASE_PATH" "$ANSIBLE_SUDO_PASS_ENTRY" --quiet --attributes Password)
+
+ANSIBLE_SUDO_PASS=$SUDOPASS ansible-playbook $PLAYBOOKS_HOME/ovh/serv-0/deactivate_forwarding.yml
 
 ssh-add -D
