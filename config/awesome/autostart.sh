79999e93a4ea32ed813e5a9f6f3398665eed87cd
Alexandre Joubert <contact@joubert.dev>
2024-09-23 18:31:39 +0200

    MINOR : fix wallpaper for multi-screen support

--- config/awesome/autostart.sh
+++ config/awesome/autostart.sh
@@ -5,7 +5,7 @@ $HOME/.local/scripts/backlight -b 0.9 &
 killall unclutter &>/dev/null; unclutter -idle 2 &
 # killall komorebi; komorebi &
 xset r rate 200 60 &
-feh --no-fehbg --bg-fill $HOME/.config/awesome/theme/wall.jpg &
+feh --no-fehbg --bg-scale $HOME/.config/awesome/theme/wall.jpg &
 ## pgrep -x cycle_wall > /dev/null || $HOME/.local/bin/cycle_wall 30m &
 ## pgrep -x polkit-gnome-au > /dev/null || /usr/libexec/polkit-gnome-authentication-agent-1 &
 ## pgrep -x greenclip > /dev/null || $HOME/.local/bin/greenclip daemon &
